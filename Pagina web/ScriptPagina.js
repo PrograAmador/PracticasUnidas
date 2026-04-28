5aa3181d4ab1678684af645188b79d808fffca1d
LAPTOP-R92VNVNM\Usuario <albertoplane96@gmail.com>
2026-04-28 11:37:31 +0200

    no message

--- Pagina web/ScriptPagina.js
+++ Pagina web/ScriptPagina.js
@@ -2,3 +2,13 @@ function showPage(pageId) {
   document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
   document.getElementById(pageId).classList.add('active');
 }
+let index = 0;
+const images = document.querySelectorAll(".carousel img");
+
+function changeSlide() {
+    images[index].classList.remove("active");
+    index = (index + 1) % images.length;
+    images[index].classList.add("active");
+}
+
+setInterval(changeSlide, 4000); // cambia cada 4s
